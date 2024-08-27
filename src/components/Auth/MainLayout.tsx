b30c461861acf03367132d32f98a94b5c6c7aa84
BoonYaPoom <101199599+BoonYaPoom@users.noreply.github.com>
2024-08-27 23:15:14 +0700

    k

--- src/components/Auth/MainLayout.tsx
+++ src/components/Auth/MainLayout.tsx
@@ -1,6 +1,5 @@
-import Header from "../Header";
+import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar";
-import Sidebar from "../Sidebar";
 
 export const MainLayout = () => {
   return (
@@ -10,7 +9,7 @@ export const MainLayout = () => {
           <Navbar />
         </div>
         <div className="w-full px-72"> 
-          <Header />
+        <Outlet/>
         </div>
       </div>
     </div>
